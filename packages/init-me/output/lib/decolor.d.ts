933f9e7cbe8c6c5ba4b961ef89e45223ecab7e81
liudaojie <liudaojie@yy.com>
2022-10-10 11:00:06 +0800

    feat: 调整 ignore 策略

--- packages/init-me/output/lib/decolor.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function decolor(ctx: string[] | string): string;
--- packages/init-me/output/lib/formatter.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function seedFull2Short(name: string): string;
-export declare function seedShort2Full(name: string): string;
